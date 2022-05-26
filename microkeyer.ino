a7698e2830727be49e73091c874594bd6764b444
Matti Pöllä <matti.polla@oikeatoliot.fi>
2022-05-26 13:21:20 +0300

    Added more codes, iambic mode

--- microkeyer.ino
+++ microkeyer.ino
@@ -38,54 +38,86 @@ unsigned long lastDebounceTime = 0;
 String buffer = "";
 
 
-char morseDecode(String s) {
+String morseDecode(String s) {
 
-    if (s == ".-") { return 'A'; }
-    if (s == "-...") { return 'B'; }
-    if (s == "-.-.") { return 'C'; }
-    if (s == "-..") { return 'D'; }
-    if (s == ".") { return 'E'; }
-    if (s == "..-.") { return 'F'; }
-    if (s == "--.") { return 'G'; }
-    if (s == "....") { return 'H'; }
-    if (s == "..") { return 'I'; }
-    if (s == ".---") { return 'J'; }
-    if (s == "-.-") { return 'K'; }
-    if (s == ".-..") { return 'L'; }
-    if (s == "--") { return 'M'; }
-    if (s == "-.") { return 'N'; }
-    if (s == "---") { return 'O'; }
-    if (s == ".--.") { return 'P'; }
-    if (s == "--.-") { return 'Q'; }
-    if (s == ".-.") { return 'R'; }
-    if (s == "...") { return 'S'; }
-    if (s == "-") { return 'T'; }
-    if (s == "..-") { return 'U'; }
-    if (s == "...-") { return 'V'; }
-    if (s == ".--") { return 'W'; }
-    if (s == ".--.") { return 'X'; }
-    if (s == "-.--") { return 'Y'; }
-    if (s == "--..") { return 'Z'; }
+  if (s == ".-") { return "A"; }
+  if (s == "-...") { return "B"; }
+  if (s == "-.-.") { return "C"; }
+  if (s == "-..") { return "D"; }
+  if (s == ".") { return "E"; }
+  if (s == "..-.") { return "F"; }
+  if (s == "--.") { return "G"; }
+  if (s == "....") { return "H"; }
+  if (s == "..") { return "I"; }
+  if (s == ".---") { return "J"; }
+  if (s == "-.-") { return "K"; }
+  if (s == ".-..") { return "L"; }
+  if (s == "--") { return "M"; }
+  if (s == "-.") { return "N"; }
+  if (s == "---") { return "O"; }
+  if (s == ".--.") { return "P"; }
+  if (s == "--.-") { return "Q"; }
+  if (s == ".-.") { return "R"; }
+  if (s == "...") { return "S"; }
+  if (s == "-") { return "T"; }
+  if (s == "..-") { return "U"; }
+  if (s == "...-") { return "V"; }
+  if (s == ".--") { return "W"; }
+  if (s == ".--.") { return "X"; }
+  if (s == "-.--") { return "Y"; }
+  if (s == "--..") { return "Z"; }
 
-    if (s == ".--.-") { return 'Å'; }
-    if (s == "-.-.") { return 'Ä'; }
-    if (s == "---.") { return 'Ö'; }
+  if (s == ".--.-") { return "Å"; }
+  if (s == "-.-.") { return "Ä"; }
+  if (s == "---.") { return "Ö"; }
 
-    if (s == ".----") { return '1'; }
-    if (s == "..---") { return '2'; }
-    if (s == "...--") { return '3'; }
-    if (s == "....-") { return '4'; }
-    if (s == ".....") { return '5'; }
-    if (s == "-....") { return '6'; }
-    if (s == "--...") { return '7'; }
-    if (s == "---..") { return '8'; }
-    if (s == "----.") { return '9'; }
-    if (s == "-----") { return '0'; }
+  if (s == ".----") { return "1"; }
+  if (s == "..---") { return "2"; }
+  if (s == "...--") { return "3"; }
+  if (s == "....-") { return "4"; }
+  if (s == ".....") { return "5"; }
+  if (s == "-....") { return "6"; }
+  if (s == "--...") { return "7"; }
+  if (s == "---..") { return "8"; }
+  if (s == "----.") { return "9"; }
+  if (s == "-----") { return "0"; }
 
-    if (s == "..--..") { return '?'; }
-    if (s == ".-.-.-") { return '.'; }
+  if (s == "........") { return "Error"; }
+  if (s == ".-...") { return "&"; }
+  if (s == ".----.") { return "\""; }
+  if (s == ".--.-.") { return "@"; }
+  if (s == "-.--.-") { return ")"; }
+  if (s == "-.--.") { return "("; }
+  if (s == "---...") { return ":"; }
+  if (s == "--..--") { return ","; }
+  if (s == "-...-") { return "="; }
+  if (s == "-.-.--") { return "!"; }
+    
+  if (s == ".-.-.-") { return "."; }
+  if (s == "-....-") { return "-"; }
+  if (s == " -..-") { return "×"; }
+  if (s == " ------..-.-----") { return "%"; }
+  if (s == ".-.-.") { return "+"; }
+  if (s == " .-..-.") { return "\""; }
+  if (s == " ..--..") { return "?"; }
+  if (s == " -..-.") { return "/"; }
 
-    return 'x';
+  // Prosigns
+  if (s == ".-.-") { return "AA"; }
+  if (s == ".-.-.") { return "AR"; }
+  if (s == ".-...") { return "AS"; }
+  if (s == "-...-.-") { return "BK"; }
+  if (s == "-...-") { return "BT"; }
+  if (s == "-.-..-..") { return "CL"; }
+  if (s == "-.-.-") { return "CT"; }
+  if (s == "-..---") { return "DO"; }
+  if (s == "-.-.-") { return "KA"; }
+  if (s == "-.--.") { return "KN"; }
+  if (s == "...-.-") { return "SK"; }
+  if (s == "...-.") { return "SN"; }
+  if (s == "...---...") { return "SOS"; }
+
+  return "x";
 }
 
 // the setup function runs once when you press reset or power the board
@@ -95,7 +127,6 @@ void setup() {
 
   pinMode(LED, OUTPUT);
   digitalWrite(LED, LOW);
-
   
   Keyboard.begin();
   pinMode(paddleLeft, INPUT);
@@ -107,6 +138,7 @@ void setup() {
 
 int space = 0;
 int newline = 1;
+int previous = 0;
 
 // the loop function runs over and over again forever
 void loop() {
@@ -131,13 +163,20 @@ void loop() {
   }
 
   paddleLeftState = digitalRead(paddleLeft);
-  paddleRightState = digitalRead(paddleRight);
+  paddleRightState = digitalRead(paddleRight);  
   
   if (((millis() - lastDebounceTime) > LETTER_SPACE_MS) && buffer.length() > 0) {
     //Serial.print(morseDecode(String(buffer)));
-    Keyboard.print(morseDecode(String(buffer)));
+    String decoded = morseDecode(String(buffer));
+    if (decoded == "Error") {
+      Keyboard.write(8);
+      space = 1;  
+    } else {
+      Keyboard.print(morseDecode(String(buffer)));
+      space = 0;
+    }
     buffer = "";
-    space = 0;
+    
     newline = 0;
     lastDebounceTime = millis();
   }
@@ -153,9 +192,10 @@ void loop() {
       digitalWrite(LED, LOW); 
       buffer += ".";
       lastDebounceTime = millis();
+      previous = 0;
     }
     // Right paddle
-    if (paddleLeftState == HIGH && paddleRightState == LOW) {
+    else if (paddleLeftState == HIGH && paddleRightState == LOW) {
       tone(BUZZER, TONE_HZ);
       digitalWrite(LED, HIGH);
       delay(DA_MS);
@@ -163,6 +203,25 @@ void loop() {
       digitalWrite(LED, LOW);
       buffer += "-";
       lastDebounceTime = millis();
-    } 
+      previous = 1;
+    }
+    // Both paddles
+    else if (paddleLeftState == LOW && paddleRightState == LOW) {
+      tone(BUZZER, TONE_HZ);
+      digitalWrite(LED, HIGH);
+      if (previous == 0) {
+        delay(DA_MS);
+        previous = 1;
+        buffer += "-";
+      } else if (previous == 1) {
+        delay(DIT_MS);
+        previous = 0;
+        buffer += ".";
+      }
+      noTone(BUZZER);
+      digitalWrite(LED, LOW);
+      lastDebounceTime = millis();
+    }
+    
   }
 }
